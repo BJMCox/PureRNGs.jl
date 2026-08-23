885c069212693d64f244b3680300cfddcdfe8564
Ben Cox <bcox@mpp.mpg.de>
2026-08-23 17:39:02 +0200

    Implement Philox core functions

--- src/philox.jl
+++ src/philox.jl
@@ -1,1 +1,82 @@
+const _PHILOX_M2X32_0 = UInt32(0xd256d193)
+const _PHILOX_M4X32_0 = UInt32(0xd2511f53)
+const _PHILOX_M4X32_1 = UInt32(0xcd9e8d57)
+const _PHILOX_W32_0 = UInt32(0x9e3779b9)
+const _PHILOX_W32_1 = UInt32(0xbb67ae85)
 
+const _PHILOX_M2X64_0 = UInt64(0xd2b74407b1ce6e93)
+const _PHILOX_M4X64_0 = UInt64(0xd2e7470ee14c6c93)
+const _PHILOX_M4X64_1 = UInt64(0xca5a826395121157)
+const _PHILOX_W64_0 = UInt64(0x9e3779b97f4a7c15)
+const _PHILOX_W64_1 = UInt64(0xbb67ae8584caa73b)
+
+@inline function _mulhilo32(a::UInt32, b::UInt32)
+    product = UInt64(a) * UInt64(b)
+    return UInt32(product >> 32), UInt32(product & 0xffffffff)
+end
+
+@inline function _mulhilo64(a::UInt64, b::UInt64)
+    mask = UInt64(0xffffffff)
+    alo, ahi = a & mask, a >> 32
+    blo, bhi = b & mask, b >> 32
+    p0 = alo * blo
+    p1 = ahi * blo
+    p2 = alo * bhi
+    p3 = ahi * bhi
+    middle = (p0 >> 32) + (p1 & mask) + (p2 & mask)
+    return p3 + (p1 >> 32) + (p2 >> 32) + (middle >> 32), p0 + (p1 << 32) + (p2 << 32)
+end
+
+@inline function _philox2x32_round(ctr::NTuple{2,UInt32}, key::NTuple{1,UInt32})
+    hi, lo = _mulhilo32(_PHILOX_M2X32_0, ctr[1])
+    return hi ⊻ ctr[2] ⊻ key[1], lo
+end
+
+@inline function _philox4x32_round(ctr::NTuple{4,UInt32}, key::NTuple{2,UInt32})
+    hi0, lo0 = _mulhilo32(_PHILOX_M4X32_0, ctr[1])
+    hi1, lo1 = _mulhilo32(_PHILOX_M4X32_1, ctr[3])
+    return hi1 ⊻ ctr[2] ⊻ key[1], lo1, hi0 ⊻ ctr[4] ⊻ key[2], lo0
+end
+
+@inline function _philox2x64_round(ctr::NTuple{2,UInt64}, key::NTuple{1,UInt64})
+    hi, lo = _mulhilo64(_PHILOX_M2X64_0, ctr[1])
+    return hi ⊻ ctr[2] ⊻ key[1], lo
+end
+
+@inline function _philox4x64_round(ctr::NTuple{4,UInt64}, key::NTuple{2,UInt64})
+    hi0, lo0 = _mulhilo64(_PHILOX_M4X64_0, ctr[1])
+    hi1, lo1 = _mulhilo64(_PHILOX_M4X64_1, ctr[3])
+    return hi1 ⊻ ctr[2] ⊻ key[1], lo1, hi0 ⊻ ctr[4] ⊻ key[2], lo0
+end
+
+@inline function _philox2x32(ctr::NTuple{2,UInt32}, key::NTuple{1,UInt32})
+    for round = 1:10
+        ctr = _philox2x32_round(ctr, key)
+        round == 10 || (key = (key[1] + _PHILOX_W32_0,))
+    end
+    return ctr
+end
+
+@inline function _philox4x32(ctr::NTuple{4,UInt32}, key::NTuple{2,UInt32})
+    for round = 1:10
+        ctr = _philox4x32_round(ctr, key)
+        round == 10 || (key = (key[1] + _PHILOX_W32_0, key[2] + _PHILOX_W32_1))
+    end
+    return ctr
+end
+
+@inline function _philox2x64(ctr::NTuple{2,UInt64}, key::NTuple{1,UInt64})
+    for round = 1:10
+        ctr = _philox2x64_round(ctr, key)
+        round == 10 || (key = (key[1] + _PHILOX_W64_0,))
+    end
+    return ctr
+end
+
+@inline function _philox4x64(ctr::NTuple{4,UInt64}, key::NTuple{2,UInt64})
+    for round = 1:10
+        ctr = _philox4x64_round(ctr, key)
+        round == 10 || (key = (key[1] + _PHILOX_W64_0, key[2] + _PHILOX_W64_1))
+    end
+    return ctr
+end
--- test/philox.jl
+++ test/philox.jl
@@ -1,1 +1,85 @@
+@testset "Philox2x32-10 KAT" begin
+    @test PureRNGs._philox2x32((0x00000000, 0x00000000), (0x00000000,)) ==
+          (0xff1dae59, 0x6cd10df2)
+    @test PureRNGs._philox2x32((0xffffffff, 0xffffffff), (0xffffffff,)) ==
+          (0x2c3f628b, 0xab4fd7ad)
+    @test PureRNGs._philox2x32((0x243f6a88, 0x85a308d3), (0x13198a2e,)) ==
+          (0xdd7ce038, 0xf62a4c12)
+end
 
+@testset "Philox4x32-10 KAT" begin
+    @test PureRNGs._philox4x32(
+        (0x00000000, 0x00000000, 0x00000000, 0x00000000),
+        (0x00000000, 0x00000000),
+    ) == (0x6627e8d5, 0xe169c58d, 0xbc57ac4c, 0x9b00dbd8)
+    @test PureRNGs._philox4x32(
+        (0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff),
+        (0xffffffff, 0xffffffff),
+    ) == (0x408f276d, 0x41c83b0e, 0xa20bc7c6, 0x6d5451fd)
+    @test PureRNGs._philox4x32(
+        (0x243f6a88, 0x85a308d3, 0x13198a2e, 0x03707344),
+        (0xa4093822, 0x299f31d0),
+    ) == (0xd16cfe09, 0x94fdcceb, 0x5001e420, 0x24126ea1)
+end
+
+@testset "Philox2x64-10 KAT" begin
+    @test PureRNGs._philox2x64(
+        (0x0000000000000000, 0x0000000000000000),
+        (0x0000000000000000,),
+    ) == (0xca00a0459843d731, 0x66c24222c9a845b5)
+    @test PureRNGs._philox2x64(
+        (0xffffffffffffffff, 0xffffffffffffffff),
+        (0xffffffffffffffff,),
+    ) == (0x65b021d60cd8310f, 0x4d02f3222f86df20)
+    @test PureRNGs._philox2x64(
+        (0x243f6a8885a308d3, 0x13198a2e03707344),
+        (0xa4093822299f31d0,),
+    ) == (0x0a5e742c2997341c, 0xb0f883d38000de5d)
+end
+
+@testset "Philox4x64-10 KAT" begin
+    @test PureRNGs._philox4x64(
+        (0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000),
+        (0x0000000000000000, 0x0000000000000000),
+    ) == (0x16554d9eca36314c, 0xdb20fe9d672d0fdc, 0xd7e772cee186176b, 0x7e68b68aec7ba23b)
+    @test PureRNGs._philox4x64(
+        (0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff),
+        (0xffffffffffffffff, 0xffffffffffffffff),
+    ) == (0x87b092c3013fe90b, 0x438c3c67be8d0224, 0x9cc7d7c69cd777b6, 0xa09caebf594f0ba0)
+    @test PureRNGs._philox4x64(
+        (0x243f6a8885a308d3, 0x13198a2e03707344, 0xa4093822299f31d0, 0x082efa98ec4e6c89),
+        (0x452821e638d01377, 0xbe5466cf34e90c6c),
+    ) == (0xa528f45403e61d95, 0x38c72dbd566e9788, 0xa5a1610e72fd18b5, 0x57bd43b5e52b7fe6)
+end
+
+@testset "Philox core inference and allocation" begin
+    let ctr = (0x243f6a88, 0x85a308d3), key = (0x13198a2e,)
+        @test @inferred(PureRNGs._philox2x32(ctr, key)) isa NTuple{2,UInt32}
+        PureRNGs._philox2x32(ctr, key)
+        @test @allocated(PureRNGs._philox2x32(ctr, key)) == 0
+    end
+    let ctr = (0x243f6a88, 0x85a308d3, 0x13198a2e, 0x03707344),
+        key = (0xa4093822, 0x299f31d0)
+
+        @test @inferred(PureRNGs._philox4x32(ctr, key)) isa NTuple{4,UInt32}
+        PureRNGs._philox4x32(ctr, key)
+        @test @allocated(PureRNGs._philox4x32(ctr, key)) == 0
+    end
+    let ctr = (0x243f6a8885a308d3, 0x13198a2e03707344), key = (0xa4093822299f31d0,)
+        @test @inferred(PureRNGs._philox2x64(ctr, key)) isa NTuple{2,UInt64}
+        PureRNGs._philox2x64(ctr, key)
+        @test @allocated(PureRNGs._philox2x64(ctr, key)) == 0
+    end
+    let ctr = (
+            0x243f6a8885a308d3,
+            0x13198a2e03707344,
+            0xa4093822299f31d0,
+            0x082efa98ec4e6c89,
+        ),
+        key = (0x452821e638d01377, 0xbe5466cf34e90c6c)
+
+        @test @inferred(PureRNGs._philox4x64(ctr, key)) isa NTuple{4,UInt64}
+        PureRNGs._philox4x64(ctr, key)
+        @test @allocated(PureRNGs._philox4x64(ctr, key)) == 0
+    end
+end
