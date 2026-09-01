652bbda57dfb3c5e9edd2869d984e6bdea667587
Ben Cox <bcox@mpp.mpg.de>
2026-09-01 12:04:29 +0200

    Remove serial range launch overhead

--- src/range_allocating.jl
+++ src/range_allocating.jl
@@ -120,7 +120,9 @@ end
     range,
     span,
 )
-    if length(destination) <= _CPU_DIRECT_SMALL_FILL_MAX_ELEMENTS
+    chunk_elements = Int(_CPU_FILL_CHUNK_BITS ÷ UInt64(_range_bits(span)))
+    workitems = cld(length(destination), chunk_elements)
+    if workitems < _CPU_FILL_MIN_WORKITEMS
         _fill_range_cpu_unchecked!(
             rng,
             rng.position,
@@ -131,8 +133,6 @@ end
         )
         return destination
     end
-    chunk_elements = Int(_CPU_FILL_CHUNK_BITS ÷ UInt64(_range_bits(span)))
-    workitems = cld(length(destination), chunk_elements)
     _range_fill_cpu_kernel!(backend)(
         rng,
         destination,
