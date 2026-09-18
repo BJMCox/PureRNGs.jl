db613c4e2006bf7b6155a4639e9cb463bea40b00
Ben Cox <bcox@mpp.mpg.de>
2026-09-18 13:23:46 +0200

    Add an error-message suite for the guards, threaded and the named values
    The closed error audit and the backend seam test move to ArgumentError for a
    non-Bool threaded, and the audit lists the six new guard methods.

--- test/closed_audits.jl
+++ test/closed_audits.jl
@@ -68,8 +68,14 @@ end
     rng = Philox4x32(0xa71)
     R = typeof(rng)
     require(rand, Tuple{R})
+    require(rand, Tuple{R,Int})
+    require(rand, Tuple{R,Tuple{Int}})
     require(randn, Tuple{R})
+    require(randn, Tuple{R,Int})
+    require(randn, Tuple{R,Tuple{Int}})
     require(randexp, Tuple{R})
+    require(randexp, Tuple{R,Int})
+    require(randexp, Tuple{R,Tuple{Int}})
     require(rand_next, Tuple{R})
     require(rand_next, Tuple{R,Int})
     require(rand_next, Tuple{R,Tuple{Int}})
@@ -295,7 +301,7 @@ end
             () -> randexp_next!(rng, wrong_exponential_destination),
         ),
     )
-    type_errors = (
+    threaded_errors = (
         (:uniform_threaded_type, () -> rand!(rng, Vector{UInt32}(undef, 1); threaded = 1)),
         (
             :uniform_continuation_threaded_type,
@@ -329,7 +335,7 @@ end
 
     for (expected, cases) in (
         (ArgumentError, argument_errors),
-        (TypeError, type_errors),
+        (ArgumentError, threaded_errors),
         (MethodError, method_errors),
     )
         for (name, call) in cases
